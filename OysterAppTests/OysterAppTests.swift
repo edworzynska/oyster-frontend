9fa330138f81f60a1928af1a54fd526e73e6d46b
edworzynska <ewa.dworzynska@icloud.com>
2024-12-16 22:57:22 +0100

    Initial Commit

---
+++ OysterAppTests/OysterAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  OysterAppTests.swift
+//  OysterAppTests
+//
+//  Created by Ewa on 16/12/2024.
+//
+
+import Testing
+@testable import OysterApp
+
+struct OysterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
